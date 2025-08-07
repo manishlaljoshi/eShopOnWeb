d51dc261482d1a2b75675924ee66c899e70b3098
DevAug Agent <devaug-agent@example.com>
2025-08-07 18:12:35 +0000

    SCRUM-6: Sort Order Detail

--- src/Web/Features/OrderDetails/GetOrderDetailsHandler.cs
+++ src/Web/Features/OrderDetails/GetOrderDetailsHandler.cs
@@ -14,9 +14,7 @@ public class GetOrderDetailsHandler : IRequestHandler<GetOrderDetails, OrderDeta
     {
         _orderRepository = orderRepository;
     }
-
-    public async Task<OrderDetailViewModel?> Handle(GetOrderDetails request,
-        CancellationToken cancellationToken)
+public async Task<OrderDetailViewModel?> Handle(GetOrderDetails request, CancellationToken cancellationToken)
     {
         var spec = new OrderWithItemsByIdSpec(request.OrderId);
         var order = await _orderRepository.FirstOrDefaultAsync(spec, cancellationToken);
@@ -26,20 +24,23 @@ public class GetOrderDetailsHandler : IRequestHandler<GetOrderDetails, OrderDeta
             return null;
         }
 
+        var items = order.OrderItems.Select(oi => new OrderItemViewModel
+        {
+            PictureUrl = oi.ItemOrdered.PictureUri,
+            ProductId = oi.ItemOrdered.CatalogItemId,
+            ProductName = oi.ItemOrdered.ProductName,
+            UnitPrice = oi.UnitPrice,
+            Units = oi.Units
+        }).ToList();
+
+        items.Sort((x, y) => y.UnitPrice.CompareTo(x.UnitPrice));
+
         return new OrderDetailViewModel
         {
             OrderDate = order.OrderDate,
-            OrderItems = order.OrderItems.Select(oi => new OrderItemViewModel
-            {
-                PictureUrl = oi.ItemOrdered.PictureUri,
-                ProductId = oi.ItemOrdered.CatalogItemId,
-                ProductName = oi.ItemOrdered.ProductName,
-                UnitPrice = oi.UnitPrice,
-                Units = oi.Units
-            }).ToList(),
+            OrderItems = items,
             OrderNumber = order.Id,
             ShippingAddress = order.ShipToAddress,
             Total = order.Total()
         };
-    }
-}
+    }}
