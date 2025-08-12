6ec1b0adc198ff19e63b7677e34bba6a969d6cbf
DevAug Agent <devaug-agent@example.com>
2025-08-12 23:32:07 +0000

    SCRUM-6: Sort Order Detail

--- src/Web/Features/OrderDetails/GetOrderDetailsHandler.cs
+++ src/Web/Features/OrderDetails/GetOrderDetailsHandler.cs
@@ -1,4 +1,4 @@
-﻿using MediatR;
+using MediatR;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
@@ -15,31 +15,30 @@ public class GetOrderDetailsHandler : IRequestHandler<GetOrderDetails, OrderDeta
         _orderRepository = orderRepository;
     }
 
-    public async Task<OrderDetailViewModel?> Handle(GetOrderDetails request,
-        CancellationToken cancellationToken)
+    public async Task<OrderDetailViewModel?> Handle(GetOrderDetails request, CancellationToken cancellationToken)
+{
+    var spec = new OrderWithItemsByIdSpec(request.OrderId);
+    var order = await _orderRepository.FirstOrDefaultAsync(spec, cancellationToken);
+
+    if (order == null)
     {
-        var spec = new OrderWithItemsByIdSpec(request.OrderId);
-        var order = await _orderRepository.FirstOrDefaultAsync(spec, cancellationToken);
-
-        if (order == null)
-        {
-            return null;
-        }
-
-        return new OrderDetailViewModel
-        {
-            OrderDate = order.OrderDate,
-            OrderItems = order.OrderItems.Select(oi => new OrderItemViewModel
-            {
-                PictureUrl = oi.ItemOrdered.PictureUri,
-                ProductId = oi.ItemOrdered.CatalogItemId,
-                ProductName = oi.ItemOrdered.ProductName,
-                UnitPrice = oi.UnitPrice,
-                Units = oi.Units
-            }).ToList(),
-            OrderNumber = order.Id,
-            ShippingAddress = order.ShipToAddress,
-            Total = order.Total()
-        };
+        return null;
     }
+
+    return new OrderDetailViewModel
+    {
+        OrderDate = order.OrderDate,
+        OrderItems = order.OrderItems.OrderByDescending(oi => oi.UnitPrice).Select(oi => new OrderItemViewModel
+        {
+            PictureUrl = oi.ItemOrdered.PictureUri,
+            ProductId = oi.ItemOrdered.CatalogItemId,
+            ProductName = oi.ItemOrdered.ProductName,
+            UnitPrice = oi.UnitPrice,
+            Units = oi.Units
+        }).ToList(),
+        OrderNumber = order.Id,
+        ShippingAddress = order.ShipToAddress,
+        Total = order.Total()
+    };
+}
 }
