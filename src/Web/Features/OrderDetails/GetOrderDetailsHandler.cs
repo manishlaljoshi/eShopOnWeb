a450a22c85c514bd7330805e40a68674e36913f6
DevAug Agent <devaug-agent@example.com>
2025-08-26 21:30:28 +0000

    SCRUM-6: Sort Order Detail

--- src/Web/Features/OrderDetails/GetOrderDetailsHandler.cs
+++ src/Web/Features/OrderDetails/GetOrderDetailsHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
 using Microsoft.eShopWeb.Web.ViewModels;
+using System.Linq;
 
 namespace Microsoft.eShopWeb.Web.Features.OrderDetails;
 
@@ -15,8 +16,7 @@ public class GetOrderDetailsHandler : IRequestHandler<GetOrderDetails, OrderDeta
         _orderRepository = orderRepository;
     }
 
-    public async Task<OrderDetailViewModel?> Handle(GetOrderDetails request,
-        CancellationToken cancellationToken)
+    public async Task<OrderDetailViewModel?> Handle(GetOrderDetails request, CancellationToken cancellationToken)
     {
         var spec = new OrderWithItemsByIdSpec(request.OrderId);
         var order = await _orderRepository.FirstOrDefaultAsync(spec, cancellationToken);
